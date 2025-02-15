edc77b086fa9ae2b9ba566880eac1b9e051dc8b6
DeathSwear <vlad0septa@yandex.ru>
2025-02-15 22:11:19 +0600

    Added custom umage network

---
+++ lib/features/global/widgets/custom_image_network.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:otherstory_app/theme/app_colors.dart';
+
+class CustomImageNetwork extends StatelessWidget {
+  const CustomImageNetwork(
+      {super.key,
+      required this.imageSource,
+      required this.width,
+      required this.height,
+      required this.clipRadius,
+      this.darken,
+      this.extraDarken});
+  final String imageSource;
+  final double width;
+  final double height;
+  final double clipRadius;
+  final bool? darken;
+  final bool? extraDarken;
+
+  Color? getDarkness() {
+    if (extraDarken != null) {
+      if (extraDarken == true) return AppColors.black45Color;
+    }
+    if (darken != null) {
+      if (darken == true) return AppColors.black25Color;
+    }
+    return null;
+  }
+
+  BlendMode? getBlendMode() {
+    if (extraDarken != null) {
+      if (extraDarken == true) return BlendMode.darken;
+    }
+    if (darken != null) {
+      if (darken == true) return BlendMode.darken;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(clipRadius),
+      child: Image.network(
+        fit: BoxFit.cover,
+        imageSource,
+        color: getDarkness(),
+        colorBlendMode: getBlendMode(),
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          } else {
+            return SizedBox(
+              width: width,
+              height: height,
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: AppColors.blackColor,
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              ),
+            );
+          }
+        },
+        errorBuilder: (context, error, stackTrace) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: AppColors.black45Color),
+            ),
+            width: width,
+            height: height,
+            child: Center(
+                child: Icon(
+              Icons.error_outline,
+              size: min(height, width) / 2,
+            )),
+          );
+        },
+        width: width,
+        height: height,
+      ),
+    );
+  }
+}
